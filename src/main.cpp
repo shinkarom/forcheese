82669b9a9be151dca7743bd414e99a517134ef48
shinkarom <shinkarom@gmail.com>
2023-05-09 10:41:47 +0300

    Basic argument parsing

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,50 @@
 #include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+#include "args.hxx"
 
 int main(int argc, char *argv[])
 {
-	std::cout<<"Hello World!";
+	args::ArgumentParser parser("Forcheese - a small Forth-like language.");
+	args::HelpFlag help(parser, "help", "Display this help menu", {'h', "help"});
+	args::Positional<string> fileName(parser, "filename", "Name of the file to run.");
+    args::CompletionFlag completion(parser, {"complete"});
+	try
+    {
+        parser.ParseCLI(argc, argv);
+    }
+    catch (const args::Completion& e)
+    {
+        std::cout << e.what();
+        return 0;
+    }
+    catch (const args::Help&)
+    {
+        std::cout << parser;
+        return 0;
+    }
+    catch (const args::ParseError& e)
+    {
+        std::cerr << e.what() << std::endl;
+        std::cerr << parser;
+        return 1;
+    }
+	catch (const args::ValidationError& e)
+    {
+        std::cerr << e.what() << std::endl;
+        std::cerr << parser;
+        return 1;
+    }
+	if(!fileName)
+	{
+		cerr<<"Error: no input file."<<endl;
+		return 1;
+	}
+	ifstream ifs;
+	ifs.open(*fileName);
+	std::cout<<"Running "<<*fileName<<"..."<<endl;
 	return 0;
 }
