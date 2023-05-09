800eff35a5141c3d9aec43f931b008424fb53d11
shinkarom <shinkarom@gmail.com>
2023-05-09 13:10:27 +0300

    Simpler argument parsing

--- src/main.cpp
+++ src/main.cpp
@@ -2,49 +2,22 @@
 #include <fstream>
 #include <string>
 
-using namespace std;
-
-#include "args.hxx"
-
 int main(int argc, char *argv[])
 {
-	args::ArgumentParser parser("Forcheese - a small Forth-like language.");
-	args::HelpFlag help(parser, "help", "Display this help menu", {'h', "help"});
-	args::Positional<string> fileName(parser, "filename", "Name of the file to run.");
-    args::CompletionFlag completion(parser, {"complete"});
-	try
-    {
-        parser.ParseCLI(argc, argv);
-    }
-    catch (const args::Completion& e)
-    {
-        std::cout << e.what();
-        return 0;
-    }
-    catch (const args::Help&)
-    {
-        std::cout << parser;
-        return 0;
-    }
-    catch (const args::ParseError& e)
-    {
-        std::cerr << e.what() << std::endl;
-        std::cerr << parser;
-        return 1;
-    }
-	catch (const args::ValidationError& e)
-    {
-        std::cerr << e.what() << std::endl;
-        std::cerr << parser;
-        return 1;
-    }
-	if(!fileName)
-	{
-		cerr<<"Error: no input file."<<endl;
+	if(argc < 2) {
+		std::cerr<<"Error: no input file."<<std::endl;
 		return 1;
 	}
-	ifstream ifs;
-	ifs.open(*fileName);
-	std::cout<<"Running "<<*fileName<<"..."<<endl;
+	std::string fileName(argv[1]);
+	
+	std::ifstream ifs;
+	ifs.open(fileName, std::fstream::in);
+	if(ifs.fail())
+	{
+		std::cerr<<"Error: couldn't open file"<<std::endl;
+		return 1;
+	}
+	std::cout<<"Running "<<fileName<<"..."<<std::endl;
+	ifs.close();
 	return 0;
 }
